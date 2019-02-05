ffad0b18488951347241a9af27ed69703ad2d101
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-05 01:48:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,14 +54,14 @@ class Dog
   end 
   
   def self.find_by_id(id)
-    # sql = <<-SQL
-    #   SELECT *
-    #   FROM dogs
-    #   WHERE id = ?
-    #   LIMIT 1
-    # SQL
-    #   row = DB[:conn].execute(sql, id).flatten
-    #   self.new_from_db(row)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+      row = DB[:conn].execute(sql, id).flatten
+      self.new_from_db(row)
   end 
   
   def self.find_or_create_by
