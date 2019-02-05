915b16cb40e132f872877aeb72e43edc5911d921
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-05 01:53:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,6 +64,8 @@ class Dog
   
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?"(name, breed)
+      if !dog.empty?
+        dog_data = dog[0]
   end 
   
  
