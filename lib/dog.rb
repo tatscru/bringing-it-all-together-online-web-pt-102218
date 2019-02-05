01594db16758f566553c12426b1a2bae327283a2
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-05 01:10:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,7 +43,11 @@ class Dog
   end 
   
   def self.find_by_id(id)
-    
+    sql = <<-SQL
+      SELECT * FROM students 
+      WHERE name = ?
+      LIMIT 1
+    SQL
   end 
   
   def self.find_or_create_by
