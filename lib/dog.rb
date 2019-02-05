50797366f68a7c9ad9c082c000039375c32626a7
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-05 02:05:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,11 @@ class Dog
     dog 
   end 
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+    SQL
     
     
   end 
