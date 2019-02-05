8e246315f967903876511d30f6395de9cd5d3e72
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-05 00:57:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ class Dog
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end 
     self 
-    #returning the instance dog 
+    #returning the instance dog class
   end
   
   def self.create
