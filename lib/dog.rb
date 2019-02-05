a131c9490f16e2cf1a6475c6c4a9e00f44b34e88
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-05 00:26:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 def Dog 
   attr_accessor :name, :breed
   
-  def initialize(name:, breed:, id = nil)
+  def initialize(:name, :breed, id = nil)
     @name = name 
     @breed = breed 
     @id = id 
