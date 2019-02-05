817def7a20a0ab0b2bda308e0cac1e213bec0fb1
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-05 01:35:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ class Dog
     
     row = DB[:conn].execute(sql, id)[0]
       self.new(row)
-      self.new(result[0], result[1], result[2])
+      self.new(row[0], result[1], result[2])
   end 
   
   def self.find_or_create_by
